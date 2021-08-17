867c81fc0476e4cfc4c59fb7d4e308b81aded5fa
roum02 <roum02@ewhain.net>
2021-08-17 16:18:26 +0900

    fix: react-dom

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 // import { useHistory } from "react-router";
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
+// const Wrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+// `;
 
 const MainPage = () => {
-  return;
-  <Wrapper>
-    <div>테스트</div>
-  </Wrapper>;
+  return <div>테스트</div>;
+
+  // <Wrapper></Wrapper>;
 };
 
 export default MainPage;
