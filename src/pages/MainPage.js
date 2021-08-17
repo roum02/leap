d82d03230f036ee64a4fbc63f0cd22a37ad914d2
roum02 <roum02@ewhain.net>
2021-08-17 16:16:16 +0900

    react-dom추가

---
+++ src/pages/MainPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+// import { useHistory } from "react-router";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const MainPage = () => {
+  return;
+  <Wrapper>
+    <div>테스트</div>
+  </Wrapper>;
+};
+
+export default MainPage;
